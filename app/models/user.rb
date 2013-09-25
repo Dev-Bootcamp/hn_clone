2303ef4df3705dca352fddc63288ce2ca501803f
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-25 10:53:49 -0500

    Models and Migrations Created

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ActiveRecord::Base
+  include BCrypt
+
+  has_many :posts
+  has_many :comments
+
+  def password=(secret)
+    return if secret == ''
+    @password = Password.create(secret)
+    self.password_digest = @password
+  end
+
+  def password
+    return unless password_digest
+    @password ||= Password.new(self.password_digest)
+  end
+
+
+  def self.authenticate(email, password)
+    user = User.find_by_email(email)
+    return user if user && (user.password == password)
+    nil  
+  end
+  
+end
